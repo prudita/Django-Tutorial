3f7984038b04f1f8cc1772320e71d5dd5efe02de
Prudita Victory <prudita.victory@ui.ac.id>
2023-03-07 10:39:38 +0700

    tutorial 3

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -4,6 +4,9 @@ from money_tracker.views import create_transaction
 from money_tracker.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from money_tracker.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from money_tracker.views import show_xml_by_id, show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
+from money_tracker.views import register #sesuaikan dengan nama fungsi yang dibuat
+from money_tracker.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from money_tracker.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 
 app_name = 'money_tracker'
@@ -14,7 +17,20 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+
+
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
+
+
+
+
+
+
 ]
 
 
